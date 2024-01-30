2056de02e23c477dceb13f770537660e01af9293
Fany1268 <150539108+Fany1268@users.noreply.github.com>
2024-01-30 16:17:42 +0100

    Update kalkulacka.js

--- kalkulacka.js
+++ kalkulacka.js
@@ -6,17 +6,11 @@
         Funkce sčítá inputy jako textové řetězce. "10" + "20" tedy vypíše "1020" ne jako 30. 
         Využijeme tedy funkce parseInt(), která nám text převede na číslo.
 
-        // využijeme funkce parseInt(), která nám text převede na číslo.
-
         function secti() {
             alert(parseInt(cislo1.value) + parseInt(cislo2.value));
         } 
               
-        Kdykoliv se v aplikaci něco stane, uživatel něco někam zadá,
-        někam klikne, JavaScript začne spouštět tzv. obsluhy událostí. Události nalezneme 
-        jako vlastnosti na jednotlivých elementech z DOM, začínají vždy na on, 
-        např. onclick je událost kliknutí.
-                
+               
         tlacitko.onclick = secti;//A onclik si vybereme na našem tlačítku a uložíme do ní naši 
         //funkci secti() a ta se spustí, když se na tlačítko klikne.
         */
